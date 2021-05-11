7f0bd01925040339147f797de526343b4f9ba22c
Frank Kair <frankkair@gmail.com>
2021-05-11 12:00:16 +0100

    Uses (preprocess (pps ppx_jane)) with let%map_open

--- bin/main.ml
+++ bin/main.ml
@@ -9,24 +9,23 @@ let command =
   Command.basic
     ~summary:"Reads My Clippings file and parses the highlights"
     ~readme:(fun () -> "More detailed information")
-    Command.Param.(
-      map (both
-             (anon ("filepath" %: string))
-             (anon (maybe ("book" %: string))))
-        ~f:(fun (filepath, book) ->
-            (fun () ->
-               let l = Library.build (In_channel.read_lines filepath) in
-               match book with
-               | Some b -> (print_quotes (Library.quotes l b));
-               | None ->
-                 let selected_book =
-                   Inquire.select "Select a book" ~options:(Library.books l)
-                   >>= fun b ->
-                   (print_quotes (Library.quotes l b));
-                   Lwt_io.printf ""
-                 in
-                 Lwt_main.run selected_book;
-            )))
+    Command.Let_syntax.(
+      let%map_open
+        filepath = anon ("filepath" %: string)
+      and book = anon (maybe ("book" %: string))
+      in
+      fun () ->
+        let l = Library.build(In_channel.read_lines filepath) in
+        match book with
+        | Some b -> print_quotes (Library.quotes l b);
+        | None ->
+          let selected_book = Inquire.select "Select a book" ~options:(Library.books l)
+            >>= fun b ->
+            print_quotes (Library.quotes l b);
+            Lwt_io.printf ""
+          in
+          Lwt_main.run selected_book;
+    )
 
 let () =
   Command.run ~version:"0.0.1" ~build_info:"First version" command
